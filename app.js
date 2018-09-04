8b59774e4771c43fbff30a1d871291d0194949a5
windra <pwindrap18@gmail.com>
2018-09-04 18:59:15 +0700

    with moongose

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/library',{useNewUrlParser:true});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const books = require('./routes/books')
+const customers = require('./routes/customers')
+const transaction = require('./routes/transactions')
 
 var app = express();
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('connected')
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,6 +27,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books',books)
+app.use('/customers',customers)
+app.use('/transactions',transaction)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,10 +1,10 @@
-const Book = require('../models/book')
+const Customer = require('../models/customer')
 const ObjectId = require('mongodb').ObjectID
 
 
 module.exports = {
-    getAllBooks : function(req,res) {
-        Book.find(function(err,docs){
+    getAllCustomers : function(req,res) {
+        Customer.find(function(err,docs){
             if (err) {
                 res.status(500).json({
                     message : err.message
@@ -12,19 +12,19 @@ module.exports = {
             } else {
                 res.status(200).json({
                     message: 'success',
-                    books : docs
+                    Customers : docs
                 })
             }
         })
     },
 
-    createBook : function(req,res) {
-        Book.create({
-            isbn: req.body.isbn,
-            title: req.body.title,
-            author: req.body.author,
-            category: req.body.category,
-            stock: req.body.stock
+    createCustomer : function(req,res) {
+        Customer.create({
+            name: req.body.name,
+            memberid: req.body.memberid,
+            address: req.body.address,
+            zipcode: req.body.zipcode,
+            phone: req.body.phone
         },function(err,docs){
             if (err) {
                 res.status(500).json({
@@ -39,13 +39,13 @@ module.exports = {
         })
     },
 
-    updateBook : function(req,res) {
-        Book.update({_id:new ObjectId(req.params.id)},{
-            isbn: req.body.isbn,
-            title: req.body.title,
-            author: req.body.author,
-            category: req.body.category,
-            stock: req.body.stock
+    updateCustomer : function(req,res) {
+        Customer.update({_id:new ObjectId(req.params.id)},{
+            name: req.body.name,
+            memberid: req.body.memberid,
+            address: req.body.address,
+            zipcode: req.body.zipcode,
+            phone: req.body.phone
         },function(err,docs){
             if (err) {
                 res.status(500).json({
@@ -60,8 +60,8 @@ module.exports = {
         })
     },
 
-    deleteBook : function(req,res) {
-        Book.remove({_id:new ObjectId(req.params.id)},function(err){
+    deleteCustomer : function(req,res) {
+        Customer.remove({_id:new ObjectId(req.params.id)},function(err){
             if (err) {
                 res.status(500).json({
                     message : err.message
